741c916d6d4d80f87dd6dcc1a379f45d282c0128
Kshitij Gupta <kshitijgm@gmail.com>
2021-02-13 19:22:04 +0530

    aosp-bulletin-scrape: Add Feb '21 patch

--- process.sh
+++ process.sh
@@ -13,6 +13,7 @@ python3 main.py --patch-level 2020-10-01 --android-version 9 --out-dir android-9
 python3 main.py --patch-level 2020-11-01 --android-version 9 --out-dir android-9/2020-11-01
 python3 main.py --patch-level 2020-12-01 --android-version 9 --out-dir android-9/2020-12-01
 python3 main.py --patch-level 2021-01-01 --android-version 9 --out-dir android-9/2021-01-01
+python3 main.py --patch-level 2021-02-01 --android-version 9 --out-dir android-9/2021-02-01
 python3 main.py --patch-level 2020-01-01 --android-version 10 --out-dir android-10/2020-01-01
 python3 main.py --patch-level 2020-02-01 --android-version 10 --out-dir android-10/2020-02-01
 python3 main.py --patch-level 2020-03-01 --android-version 10 --out-dir android-10/2020-03-01
@@ -26,6 +27,7 @@ python3 main.py --patch-level 2020-10-01 --android-version 10 --out-dir android-
 python3 main.py --patch-level 2020-11-01 --android-version 10 --out-dir android-10/2020-11-01
 python3 main.py --patch-level 2020-12-01 --android-version 10 --out-dir android-10/2020-12-01
 python3 main.py --patch-level 2021-01-01 --android-version 10 --out-dir android-10/2021-01-01
+python3 main.py --patch-level 2021-02-01 --android-version 10 --out-dir android-10/2021-02-01
 python3 main.py --patch-level 2020-01-01 --android-version 11 --out-dir android-11/2020-01-01
 python3 main.py --patch-level 2020-02-01 --android-version 11 --out-dir android-11/2020-02-01
 python3 main.py --patch-level 2020-03-01 --android-version 11 --out-dir android-11/2020-03-01
@@ -39,6 +41,7 @@ python3 main.py --patch-level 2020-10-01 --android-version 11 --out-dir android-
 python3 main.py --patch-level 2020-11-01 --android-version 11 --out-dir android-11/2020-11-01
 python3 main.py --patch-level 2020-12-01 --android-version 11 --out-dir android-11/2020-12-01
 python3 main.py --patch-level 2021-01-01 --android-version 11 --out-dir android-11/2021-01-01
+python3 main.py --patch-level 2021-02-01 --android-version 10 --out-dir android-10/2021-02-01
 python3 main.py --patch-level 2020-01-01 --out-dir common/2020-01-01
 python3 main.py --patch-level 2020-02-01 --out-dir common/2020-02-01
 python3 main.py --patch-level 2020-03-01 --out-dir common/2020-03-01
@@ -52,3 +55,4 @@ python3 main.py --patch-level 2020-10-01 --out-dir common/2020-10-01
 python3 main.py --patch-level 2020-11-01 --out-dir common/2020-11-01
 python3 main.py --patch-level 2020-12-01 --out-dir common/2020-12-01
 python3 main.py --patch-level 2021-01-01 --out-dir common/2021-01-01
+python3 main.py --patch-level 2021-02-01 --out-dir common/2021-02-01
